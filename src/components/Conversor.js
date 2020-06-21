6a46084776cd3c0f3eeeaca5cf7d8d541894f8c1
wellingtonmota13 <impeto.wellington@gmail.com>
2020-06-21 00:51:33 -0300

    Adicionando conexão com api free currency

--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -6,9 +6,29 @@ export default class Conversor extends Component {
         super(props);
 
         this.state = {
-            moedaA_vamor:"",
+            moedaA_valor :"",
             moedaB_valor:0,
         }
+
+        this.converter = this.converter.bind(this);
+    } 
+
+
+    converter(){
+
+        let de_para = `${this.props.moedaA}_${this.props.moedaB}`;
+
+        let url = `http://free.currencyconverterapi.com/api/v5/convert?q=${de_para}&compact=y`
+
+        fetch(url).then(res=>{
+            return res.jason()
+        }).then(jason=>{
+            let cotacao = jason[de_para].val;
+            let moedaB_valor = parseFloat(this.state.moedaA_valor) * cotacao.toFixed(2);
+            this.setState({moedaB_valor});
+        })
+
+        
     }
 
 
@@ -18,9 +38,9 @@ export default class Conversor extends Component {
     return (
       <div className="conversor">
           <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
-          <input type="text"></input>
-          <input type="button" value="Converter"></input>
-          <h2>Valor convertido</h2>
+          <input type="text" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
+          <input type="button" value="Converter" onClick={this.converter}></input>
+          <h2>{this.state.moedaB_valor }</h2>
       </div>
     );
   }
